695f760307a381273228147bbdaf99dc5d3e7737
rohini953 <rohinisk77@gmail.com>
2026-06-19 09:05:13 +0530

    Add files via upload

---
+++ Exp.16.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    'school_code': ['s001', 's002', 's001', 's002', 's003'],
+    'name': ['Ram', 'Sam', 'Ravi', 'Kumar', 'John'],
+    'age': [15, 16, 14, 15, 17]
+}
+
+df = pd.DataFrame(data)
+
+grouped = df.groupby('school_code')
+
+print(grouped)
+print(type(grouped))
